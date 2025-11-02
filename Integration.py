80f8445ef15310210d280b8607cfd13da42e0e7b
Aaron HYM <jennywang094@gmail.com>
2025-11-02 19:08:58 +0800

    PRAC-4 SRUM04_Software_Upading

--- Integration.py
+++ Integration.py
@@ -6,7 +6,7 @@ import sqlite3
 # basic GUI 
 root = tk.Tk()
 root.title('INTEGRATION')
-root.geometry('300x350')
+root.geometry('300x400')
 
 # new label and inpu
 # student ID label and entry
@@ -61,8 +61,8 @@ def overview_student():
     print (overview)
 
 # new botton Overview
-botton_overview = tk.Button(root, text='Overview', command=overview_student)
-botton_overview.pack(pady=25)
+button_overview = tk.Button(root, text='Overview', command=overview_student)
+button_overview.pack(pady=25)
 
 def delete_student():
     student_id = entry_id.get()
@@ -71,5 +71,8 @@ def delete_student():
     cursor.execute('DELETE from DB_student where db_student_id = ?',(student_id,))
     print('Following row is delete:', delete)
     conn.commit()
+
+button_delete = tk.Button(root, text='Delete', command=delete_student)
+button_delete.pack(pady=25) 
 
 root.mainloop() #must be put to the end of programming code
